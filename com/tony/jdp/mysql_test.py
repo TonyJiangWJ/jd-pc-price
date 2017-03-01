e142e9c4bb17058049acb12d976a292bb7647127
tonyjiangwj <tonyjiangwj@gmail.com>
2017-03-01 10:16:32 +0800

    change error

--- com/tony/jdp/mysql_test.py
+++ com/tony/jdp/mysql_test.py
@@ -5,7 +5,8 @@ conn = pymysql.connect(host='localhost',port=3306,
            user='root',passwd='fixture',db='jd_price',charset='UTF8')
 cur = conn.cursor()
 cur.execute("select version()")
-print(cur.data)
+data = cur.fetchone()
+print(data)
 cur.close();
 conn.close();
 
